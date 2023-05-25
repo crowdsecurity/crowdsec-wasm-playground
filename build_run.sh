bf879b5b3b8c637be3044e65b09dc1b3d2a8ecff
bui <thibault@crowdsec.net>
2023-05-25 10:17:46 +0200

    imp

--- build_run.sh
+++ build_run.sh
@@ -1,8 +1,9 @@
+rm -rf ./server &&
 go build -o ./server ./http/main.go && \
 rm -rf ./main.wasm && \
-cd ./wasm && rm -rf ./main.wasm && \
+cd ./wasm && \
 GOOS=js GOARCH=wasm go build -o main.wasm && \
-cp ./main.wasm ../ && \
+mv ./main.wasm ../ && \
 cd .. && \
 cp "$(go env GOROOT)/misc/wasm/wasm_exec.js" . && \
 ./server
