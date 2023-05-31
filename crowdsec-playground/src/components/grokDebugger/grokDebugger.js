192ab1d49f7d2a8d88ffc1a354c947ea36f05eed
Sebastien Blot <sebastien@crowdsec.net>
2023-05-31 18:10:41 +0200

    fix build warning

--- crowdsec-playground/src/components/grokDebugger/grokDebugger.js
+++ crowdsec-playground/src/components/grokDebugger/grokDebugger.js
@@ -16,10 +16,10 @@ const StyledTextarea = styled(TextareaAutosize)({
 	padding: '12px 24px',
 	border: '1px solid #ccc',
 	'&:focus': {
-	  borderColor: 'black',
-	  outline: 'none',
+		borderColor: 'black',
+		outline: 'none',
 	},
-  });
+});
 
 /*
 console.log("idx => ", idx , " type => ", typeof idx)
@@ -39,47 +39,46 @@ function renderPattern(end_idx, pattern) {
 	let submatch_idx = 0
 
 	//-1 means full match
-	if (end_idx == -1) {
+	if (end_idx === -1) {
 		console.log("full match!")
 		end_idx = pattern.length
 	}
 
-	renderchar:
 	for (let i = 0; i < pattern.length; i++) {
 		//console.log("char[%d] = %s", i, pattern[i])
 		if (i > end_idx) {
 			//console.log("after pattern")
 			patternStyles.push(
-				{ text: pattern[i], style: { color: 'red', fontWeight: 'bold'} }
+				{ text: pattern[i], style: { color: 'red', fontWeight: 'bold' } }
 			)
-			continue renderchar
+			continue
 		}
 
 		//console.log("substring => %s", pattern.substring(i-2, i))
 		//identify if we're in or out of a submatch
-		if (pattern[i] == "%" && pattern[i+1] == "{") {
+		if (pattern[i] === "%" && pattern[i + 1] === "{") {
 			//console.log("entering submatch %d", submatch_idx)
 			insubmatch = true
 		}
 
-		if (insubmatch ==  true && pattern[i-1] == "}" && pattern[i-2] != "\\") {
+		if (insubmatch === true && pattern[i - 1] === "}" && pattern[i - 2] !== "\\") {
 			insubmatch = false
 			//console.log("leaving submatch %d", submatch_idx)
 			submatch_idx++
 		}
 
-		if (insubmatch == true) {
+		if (insubmatch === true) {
 			//console.log("in submatch %d", submatch_idx)
 			patternStyles.push(
-				{ text: pattern[i], style: { color: 'green', fontWeight: 'bold', backgroundColor: submatch_group_colors[submatch_idx]} }
+				{ text: pattern[i], style: { color: 'green', fontWeight: 'bold', backgroundColor: submatch_group_colors[submatch_idx] } }
 			)
-			continue renderchar
+			continue
 		}
 
 		//we're not in a submatch, but we're matched
 		//console.log("in match")
 		patternStyles.push(
-			{ text: pattern[i], style: { color: 'green', fontWeight: 'bold'} }
+			{ text: pattern[i], style: { color: 'green', fontWeight: 'bold' } }
 		)
 	}
 	return patternStyles
@@ -88,30 +87,30 @@ function renderPattern(end_idx, pattern) {
 //render the text with the correct color
 function renderText(start_idx, end_idx, submatch_idx, text) {
 	let dataStyles = []
-	renderchar:
+
 	for (let i = 0; i < text.length; i++) {
 		//The char isn't matched
 		if (i < start_idx || i >= end_idx) {
 			dataStyles.push(
-				{ text: text[i], style: { color: 'red', fontWeight: 'bold'} }
+				{ text: text[i], style: { color: 'red', fontWeight: 'bold' } }
 			)
-			continue renderchar
+			continue
 		}
 		//Is the char part of a submatch ?
-		let submatch = false
+		//let submatch = false
 		for (let j = 0; j < submatch_idx.length; j++) {
 			if (i >= submatch_idx[j][0] && i < submatch_idx[j][1]) {
 				dataStyles.push(
 					{ text: text[i], style: { color: 'green', fontWeight: 'bold', backgroundColor: submatch_group_colors[j] } }
 				)
-				continue renderchar
+				continue
 			}
 		}
 		//The char is matched, but not part of a submatch
 		dataStyles.push(
-			{ text: text[i], style: { color: 'green', fontWeight: 'bold'} }
+			{ text: text[i], style: { color: 'green', fontWeight: 'bold' } }
 		)
-		
+
 	}
 	return dataStyles
 }
@@ -121,8 +120,6 @@ function GrokDebugger() {
 	//const [patternValue, setPatternValue] = React.useState('');
 	//const [inputValue, setInputValue] = React.useState('');
 	const [outputDictValue, setOutputDictValue] = React.useState('');
-	const [matchedValue, setMatchedValue] = React.useState('');
-	const [unmatchedValue, setUnmatchedValue] = React.useState('');
 	const [error, setError] = React.useState('');
 	const [grokStyles, setGrokStyles] = React.useState([]);
 	const [dataStyles, setDataStyles] = React.useState([]);
@@ -163,61 +160,59 @@ function GrokDebugger() {
 			delete ret["__submatches_idx"]
 		}
 
-		console.log("idx => ", idx , " type => ", typeof idx)
-		console.log("ret => ", ret , " type => ", typeof ret)
-		console.log("error => ", error , " type => ", typeof error)
-		console.log("start_idx => ", start_idx , " type => ", typeof start_idx)
-		console.log("end_idx => ", end_idx , " type => ", typeof end_idx)
-		console.log("submatch indexes => ", submatch_indexes , " type => ", typeof submatch_indexes)
+		console.log("idx => ", idx, " type => ", typeof idx)
+		console.log("ret => ", ret, " type => ", typeof ret)
+		console.log("error => ", error, " type => ", typeof error)
+		console.log("start_idx => ", start_idx, " type => ", typeof start_idx)
+		console.log("end_idx => ", end_idx, " type => ", typeof end_idx)
+		console.log("submatch indexes => ", submatch_indexes, " type => ", typeof submatch_indexes)
 
-		if (idx != undefined) {
-		if (ret !== undefined) {
-		 	setOutputDictValue(JSON.stringify(ret, null, 2))
-		 }
+		if (idx !== undefined) {
+			if (ret !== undefined) {
+				setOutputDictValue(JSON.stringify(ret, null, 2))
+			}
 
-		setDataStyles( renderText(start_idx, end_idx, submatch_indexes, inputValue.current) )
-		setGrokStyles( renderPattern(idx, patternValue.current) )
+			setDataStyles(renderText(start_idx, end_idx, submatch_indexes, inputValue.current))
+			setGrokStyles(renderPattern(idx, patternValue.current))
 		}
 	}
 
 	return (
 		<Grid container spacing={2}>
-		<Grid item xs={8} md={8}>
-		  <Item>
-		  <div>
-		{error && <Alert severity="error">An error occured while processing data: {error}.</Alert>}
-		<div align="left"><h1>Pattern</h1></div>
-		<StyledTextarea
-  			minRows={1}
-			className="fixed-textarea"
-  			placeholder="Grok Pattern"
-			onChange={handlePatternChange}
-		/>
-		<div align="left"><h1>Test Data</h1></div>
-		<StyledTextarea
- 		minRows={3}
-		className='fixed-textarea'
-  		placeholder="Input"
-		onChange={handleInputChange}
-		/>
-		<div><Button variant="contained" onClick={HandleClick}>Run</Button></div>
-		<h1>Output</h1>
-		<div>{outputDictValue}</div>
-		<div className='matched'>{matchedValue}</div>
-		<div className='unmatched'>{unmatchedValue}</div>
-		<h2> Grok Pattern results </h2>
-		<div className='grokResult'><RichTextDisplay styles={grokStyles}/></div>
-		<h2> Match data results </h2>
-		<div className='dataResult'><RichTextDisplay styles={dataStyles}/></div>
-		</div>
-		  </Item>
+			<Grid item xs={8} md={8}>
+				<Item>
+					<div>
+						{error && <Alert severity="error">An error occured while processing data: {error}.</Alert>}
+						<div align="left"><h1>Pattern</h1></div>
+						<StyledTextarea
+							minRows={1}
+							className="fixed-textarea"
+							placeholder="Grok Pattern"
+							onChange={handlePatternChange}
+						/>
+						<div align="left"><h1>Test Data</h1></div>
+						<StyledTextarea
+							minRows={3}
+							className='fixed-textarea'
+							placeholder="Input"
+							onChange={handleInputChange}
+						/>
+						<div><Button variant="contained" onClick={HandleClick}>Run</Button></div>
+						<h1>Output</h1>
+						<div>{outputDictValue}</div>
+						<h2> Grok Pattern results </h2>
+						<div className='grokResult'><RichTextDisplay styles={grokStyles} /></div>
+						<h2> Match data results </h2>
+						<div className='dataResult'><RichTextDisplay styles={dataStyles} /></div>
+					</div>
+				</Item>
+			</Grid>
+			<Grid item xs={4} md={4}>
+				<Item><GrokLibrary /></Item>
+			</Grid>
 		</Grid>
-		<Grid item xs={4} md={4}>
-		  <Item><GrokLibrary/></Item>
-		</Grid>
-	</Grid>
 
-		
+
 	);
 }
 
--- crowdsec-playground/src/components/header/header.tsx
+++ crowdsec-playground/src/components/header/header.tsx
@@ -3,12 +3,10 @@ import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
-import Menu from '@mui/material/Menu';
 import Container from '@mui/material/Container';
 import Button from '@mui/material/Button';
-import MenuItem from '@mui/material/MenuItem';
 
-import {ReactComponent as CrowdsecLogo} from '../../crowdsec.svg';
+import { ReactComponent as CrowdsecLogo } from '../../crowdsec.svg';
 import { Link } from 'react-router-dom';
 
 const pages = {
@@ -19,7 +17,6 @@ const pages = {
 }
 
 function ResponsiveAppBar() {
-  const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(null);
 
   return (
     <AppBar position="static">
@@ -44,35 +41,10 @@ function ResponsiveAppBar() {
             Crowdsec Playground
           </Typography>
 
-          <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
-            <Menu
-              id="menu-appbar"
-              anchorEl={anchorElNav}
-              anchorOrigin={{
-                vertical: 'bottom',
-                horizontal: 'left',
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: 'top',
-                horizontal: 'left',
-              }}
-              open={Boolean(anchorElNav)}
-              sx={{
-                display: { xs: 'block', md: 'none' },
-              }}
-            >
-              {Object.keys(pages).map((page, index) => (
-                <MenuItem divider={true} component={Link} to={pages[page]} key={page}>
-                  <Typography textAlign="center">{page}</Typography>
-                </MenuItem>
-              ))}
-            </Menu>
-          </Box>
           <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
             {Object.keys(pages).map((page) => (
               <Button
-              component={Link} to={pages[page]} key={page}
+                component={Link} to={pages[page]} key={page}
                 sx={{ my: 2, color: 'white', display: 'block' }}
               >
                 {page}
