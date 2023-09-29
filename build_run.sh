9a25bc8fcf7a369a8b6b2059e7b4329e4559e1db
Clement Aceituno <10882592+clementAC@users.noreply.github.com>
2023-09-29 16:44:13 +0200

    clean unused artifacts

--- build_run.sh
+++ build_run.sh
@@ -2,4 +2,4 @@ cd ./wasm && \
 GOOS=js GOARCH=wasm go build -o ../crowdsec-playground/public/main.wasm && \
 cd ../crowdsec-playground/ && \
 cp "$(go env GOROOT)/misc/wasm/wasm_exec.js" ./public/ && \
-PUBLIC_URL=/ npm start
+PUBLIC_URL=/ NODE_ENV="development" npm start
